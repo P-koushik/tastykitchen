07d9283e9d04b5bda482f980bfa3be328364c661
Your Name <you@example.com>
2025-06-14 14:57:54 +0530

    change loginpage.css to Loginpage.css

--- src/components/loginpage/Loginpage.jsx
+++ src/components/loginpage/Loginpage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './loginpage.css'
+import './Loginpage.css'
 import Cookies from "js-cookie";
 import {useState,useEffect} from 'react'
 import { useNavigate } from 'react-router-dom'
